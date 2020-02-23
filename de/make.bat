864d429a390c599894b03a6fb547737c488bba48
Simon Dückert <simon.dueckert@cogneon.de>
2020-02-23 09:59:00 +0100

    Inhlate aus Benchlearning Whitebook übernommen, Markdown-Formatierung angepasst
    Google Doc nach Word (docx) nach Markdown hat zu viel Nacharbeit notwendig gemacht, finaler Weg war dann Google Doc nach Text (txt) und alle Formatierungen in Markdown nachgezogen.

--- de/make.bat
+++ de/make.bat
@@ -2,31 +2,40 @@
 echo Starting lernOS Guide Generation ...
 
 REM Required Software
-REM Calibre, includes ebook-convert, https://calibre-ebook.com
-REM Ghostscript, https://www.ghostscript.com (version 9.24, version 9.26 does not work!)
-REM ImageMagick, https://www.imagemagick.org
-REM MiKTeX, https://miktex.org
-REM Pandoc, https://pandoc.org
-REM Wandmalfarbe Pandoc Template, https://github.com/Wandmalfarbe/pandoc-latex-template
+REM See lernOS Core Repository
 
 REM Variables
 set filename="lernOS-Community-Management-Guide-de"
+set chapters=./src/index.md ./src/lernOS-Community-Management-Guide-de.md
 
 REM Delete Old Versions
-del %filename%.docx %filename%.epub %filename%.mobi %filename%.html %filename%.pdf images\ebook-cover.png
+echo Deleting old versions ...
+del %filename%.docx %filename%.epub %filename%.mobi %filename%.html %filename%.pdf
 
 REM Create Microsoft Word Version (docx)
-pandoc -s -o %filename%.docx %filename%.md
+echo Creating Word version ...
+pandoc metadata.yaml -s --resource-path="./src" %chapters% -o %filename%.docx
 
-REM Create Web Version (html)
-pandoc -s --toc -o %filename%.html %filename%.md
+REM Create HTML Version (html)
+echo Creating HTML version ...
+pandoc metadata.yaml -s --resource-path="./src" --toc %chapters% -o %filename%.html
+
+REM Create Web Version (mkdocs)
+echo Creating Web Version ...
+mkdocs build
 
 REM Create PDF Version (pdf)
-pandoc %filename%.md metadata/metadata.yaml -o %filename%.pdf --from markdown --template lernOS --number-sections -V lang=de-de
+echo Creating PDF version ...
+pandoc metadata.yaml --from markdown --resource-path="./src" --template lernOS --number-sections -V lang=de-de %chapters% -o %filename%.pdf 
 
 REM Create eBook Versions (epub, mobi)
-magick -density 300 %filename%.pdf[0] images/ebook-cover.png
-pandoc -s --epub-cover-image=images/ebook-cover.png -o %filename%.epub %filename%.md
+echo Creating eBook versions ...
+magick -density 300 %filename%.pdf[0] src/images/ebook-cover.jpg
+magick mogrify -size 2500x2500 -resize 2500x2500 src/images/ebook-cover.jpg
+magick mogrify -crop 1563x2500+102+0 src/images/ebook-cover.jpg
+pandoc metadata.yaml -s --resource-path="./src" --epub-cover-image=src/images/ebook-cover.jpg %chapters% -o %filename%.epub
 ebook-convert %filename%.epub %filename%.mobi
+
+echo Done. Check for error messages or warnings above. 
 
 pause
